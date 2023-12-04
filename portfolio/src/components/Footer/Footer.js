69ffe5eb908986686b071bbb532fbfdbe9036cdd
Pratik <shringarpure95@gmail.com>
2023-12-04 18:51:18 +0530

    first commit

---
+++ portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/de1uze'
+      className='link footer__link'
+    >
+     Thank you 
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ portfolio/src/portfolio.js
@@ -0,0 +1,76 @@
+const header = {
+
+ 
+  title: 'JS.',
+}
+
+const about = {
+  
+  name: 'Pratik Shringarpure',
+  role: 'Front End Engineer',
+  description:
+    'Im Pratik Shringarpure.I am a recent graduate from Nutan College of Engineering in Pune, specializing in Front-End Development. With a strong foundation in HTML, CSS, and React, I am passionate about leveraging these skills to create visually stunning and user-friendly websites.',
+  resume: 'https://drive.google.com/file/d/1OTwRfawYFCC9gOO9iLRrwyZ7W0wWN11e/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/pratik9423/',
+    github: 'https://github.com/de1uze',
+  },
+}
+
+const projects = [
+  
+  {
+    name: 'Student Ed-Tech Platform',
+    description:
+      'A simple Student Dashboard with Login and Sign-up functionality. A replica of ZEN Class Student Dashboard using React',
+    stack: ['CSS', 'JavaScript', 'React'],
+    sourceCode: 'https://github.com/de1uze/Zenclass-Dashboard',
+    livePreview: 'https://zenclass-dashboard.vercel.app/',
+  },
+  {
+    name: 'Foodlify | A Recipe Suggestion Website',
+    description:
+      'College Capstone Project | A Realtime recipe and dish suggestion built on React and Mongo DB.Uses Python machine learning collaboration for recipe.      ',
+    stack: ['Javascript', 'React', 'Machine Learning'],
+    sourceCode: 'https://github.com/de1uze/Foodlify',
+    livePreview: 'https://foodlify-omega.vercel.app/',
+  },
+  {
+    name: 'A Gym website    ',
+    description:
+      'A website for gym. Displaying trainers, pricing, environment made using React',
+    stack: ['CSS', 'JavaScript', 'React'],
+    sourceCode: 'https://github.com/de1uze/Gym-rat',
+    livePreview: 'https://gym-rat-deploy.onrender.com/',
+  },
+  {
+    name: 'Simple Netflix Clone     ',
+    description:
+      'Tutorial Based Netflix clone. Fetching data for Movie API',
+    stack: ['API', 'JavaScript', 'React'],
+    sourceCode: 'https://github.com/de1uze/Ntflx-clone',
+    livePreview: 'https://ntflx-clone-ivory.vercel.app/',
+  },
+]
+
+const skills = [
+  
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Mongo DB',
+  'Git',
+  'Express JS',
+  'Node JS',
+  
+]
+
+const contact = {
+  
+  email: 'shringarpure95@gmail.com',
+ 
+}
+
+export { header, about, projects, skills, contact }
